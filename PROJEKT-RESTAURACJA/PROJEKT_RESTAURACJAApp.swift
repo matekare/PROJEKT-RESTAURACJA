4f12c18d1e845a0e79b4b0ecca522e4422992570
matekare <mateusz.macher@gmail.com>
2024-01-26 18:42:26 +0100

    Initial Commit

---
+++ PROJEKT-RESTAURACJA/PROJEKT_RESTAURACJAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROJEKT_RESTAURACJAApp.swift
+//  PROJEKT-RESTAURACJA
+//
+//  Created by User on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PROJEKT_RESTAURACJAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
